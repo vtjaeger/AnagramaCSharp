9c00a1e4c8624549cd4e8c2414661989dbc4759b
vtjaeger <viniciustjaeger@gmail.com>
2023-07-26 16:01:33 -0300

    Anagrama com C#

---
+++ AnagramaP2/Class/Anagram.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramaP2.Class;
+
+public class Anagram
+{
+    public static Dictionary<char, int> ContarRepeticoes(string palavra)
+    {
+        Dictionary<char, int> repeticoes = new Dictionary<char, int>();
+
+        foreach (char valor in palavra)
+        {
+            if (repeticoes.ContainsKey(valor))
+            {
+                repeticoes[valor]++;
+            }
+            else
+            {
+                repeticoes[valor] = 1;
+            }
+        }
+        return repeticoes;
+    }
+
+
+
+    public static string RemoverRepeticoes(string palavra)
+    {
+        List<char> valorUnico = new List<char>();
+
+        foreach (char valor in palavra)
+        {
+            if (!valorUnico.Contains(valor))
+            {
+                valorUnico.Add(valor);  
+            }
+        }
+        return new string(valorUnico.ToArray());
+    }
+
+    public static int CalcularFatorial(int numero)
+    {
+        if(numero <= 1)
+        {
+            return 1;
+        }
+
+        int resultado = 1;
+
+        for(int i = 2; i <= numero; i++)
+        {
+            resultado *= i;
+        }
+        return resultado;
+    }
+    public static int CalculaAnagrama(string palavra)
+    {
+        int totalDePossibilidades = CalcularFatorial((palavra.Length));
+        Dictionary<char, int> repeticoes = ContarRepeticoes(palavra);
+
+        foreach (int valor in repeticoes.Values)
+        {
+            totalDePossibilidades /= CalcularFatorial(valor);
+        }
+        return totalDePossibilidades;
+    }
+}
---
+++ AnagramaP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using AnagramaP2.Class;
+using System.Reflection.Metadata.Ecma335;
+
+while (true)
+{
+    Console.WriteLine("BEM VINDO\n");
+    Console.WriteLine("Escreva uma palavra:");
+
+    string palavra = Console.ReadLine()!;
+    Console.WriteLine($"\nÉ possível escrever {palavra} de {Anagram.CalculaAnagrama(palavra)} formas diferentes.");
+    Thread.Sleep(2000);
+
+    Console.WriteLine("\nPara continuar aperte 1.\nCaso queira sair aperte qualquer outra tecla.");
+    string opcao = Console.ReadLine()!;
+    switch (opcao)
+    {
+        case "1":
+            Console.Clear();
+            break;
+        default:
+            Thread.Sleep(150);
+            Console.Write($"Saindo");
+            Thread.Sleep(150);
+            Console.Write(" do");
+            Thread.Sleep(150);
+            Console.Write(" programa...");
+            Thread.Sleep(400);
+            return;
+    }
+}
+
+
